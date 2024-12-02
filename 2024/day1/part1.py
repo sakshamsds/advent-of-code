7420ee918b1aca01f35bc9dd809e4a9077edd7ae
Sakshamdeep Singh <saksham.sds@gmail.com>
2024-12-01 21:23:20 -0500

    add solutions

---
+++ 2024/day1/part1.py
@@ -0,0 +1,22 @@
+import re
+
+with open('./input.txt') as f:
+    lines = f.readlines()
+
+number_pattern = re.compile(r'\d+')
+
+res = 0
+l1, l2 = [], []
+for line in lines:
+    nums = number_pattern.findall(line)
+    l1.append(int(nums[0]))
+    l2.append(int(nums[1]))
+
+l1.sort()
+l2.sort()
+
+total_distance = 0
+for n1, n2 in zip(l1, l2):
+    total_distance += abs(n2 - n1)
+
+print(total_distance)
---
+++ 2024/day1/part2.py
@@ -0,0 +1,20 @@
+import re
+import collections
+
+with open('./input.txt') as f:
+    lines = f.readlines()
+
+number_pattern = re.compile(r'\d+')
+
+res = 0
+l1, l2_freqs = [], collections.defaultdict(int)
+for line in lines:
+    nums = number_pattern.findall(line)
+    l1.append(int(nums[0]))
+    l2_freqs[int(nums[1])] += 1
+
+similarity_score = 0
+for num in l1:
+    similarity_score += num * l2_freqs[num]
+
+print(similarity_score)
